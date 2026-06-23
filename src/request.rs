5b62182d2630a3fa695e3028e8ead6eae75836b2
shemaikuzwe <eshemaikuzwe@gmail.com>
2026-06-23 14:21:29 +0200

    fix request reader

--- src/request.rs
+++ src/request.rs
@@ -18,11 +18,12 @@ pub struct RequestLine {
     pub method: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum ParseState {
     INIT,
     HEADERS,
     BODY,
+    DONE,
 }
 
 #[derive(Debug, PartialEq)]
@@ -45,6 +46,9 @@ impl Request {
             request_line: None,
         }
     }
+    fn done(&self) -> bool {
+        self.parse_state == ParseState::DONE
+    }
     pub fn parse(&mut self, data: &[u8]) -> Result<usize, ParseError> {
         let mut read = 0;
         loop {
@@ -54,7 +58,11 @@ impl Request {
             }
             match self.parse_state {
                 ParseState::INIT => {
-                    let result = parse_request_line(curr_data)?;
+                    let result = match parse_request_line(curr_data) {
+                        Ok(result) => result,
+                        Err(ParseError::IncompleteRequestLine) => return Ok(read),
+                        Err(err) => return Err(err),
+                    };
                     if result.1 == 0 {
                         return Ok(result.1);
                     }
@@ -64,6 +72,9 @@ impl Request {
                 }
                 ParseState::HEADERS => {
                     let result = self.headers.parse(curr_data)?;
+                    if result.0 == 0 {
+                        return Ok(read);
+                    }
                     read += result.0;
                     if result.1 {
                         self.parse_state = ParseState::BODY
@@ -77,45 +88,56 @@ impl Request {
                         .parse::<usize>()
                         .map_err(|_| ParseError::InvalidContentLengthHeader)?;
                     if length == 0 {
+                        self.parse_state = ParseState::DONE;
                         return Ok(read);
                     }
                     let remaining = min(curr_data.len(), length - self.body.len());
                     self.body.extend_from_slice(&curr_data[..remaining]);
                     read += remaining;
-                    if self.body.len() == remaining {
+                    if self.body.len() == length {
+                        self.parse_state = ParseState::DONE;
                         return Ok(read);
                     }
                     if remaining == 0 && self.body.len() != length {
                         return Err(ParseError::InvalidContentLengthHeader);
                     }
                 }
+                ParseState::DONE => {
+                    println!("request parsing completed {:#?}", self);
+                }
             }
         }
     }
 }
 #[derive(Debug)]
-pub enum RequestReadError {
+pub enum RequestError {
     Io(io::Error),
     Parse(ParseError),
     RequestTooLarge,
     UnexexpectedEndOfInput,
 }
-pub fn request_from_reader<R: Read>(reader: &mut R) -> Result<Request, RequestReadError> {
+pub fn request_from_reader<R: Read>(reader: &mut R) -> Result<Request, RequestError> {
     let mut request = Request::new();
     let mut buffer = [0_u8; 1024];
     let mut buffer_index = 0;
-    loop {
+    while !request.done() {
         if buffer_index == buffer.len() {
-            return Err(RequestReadError::RequestTooLarge);
+            return Err(RequestError::RequestTooLarge);
         }
         let bytes_read = reader
             .read(&mut buffer[buffer_index..])
-            .map_err(RequestReadError::Io)?;
+            .map_err(RequestError::Io)?;
         if bytes_read == 0 {
-            return Err(RequestReadError::UnexexpectedEndOfInput);
+            return Err(RequestError::UnexexpectedEndOfInput);
         }
-        let read = request.parse(&buffer);
+        buffer_index += bytes_read;
+        let read = request
+            .parse(&buffer[..buffer_index])
+            .map_err(RequestError::Parse)?;
+        buffer.copy_within(read..buffer_index, 0);
+        buffer_index -= read
     }
+    Ok(request)
 }
 
 fn parse_request_line(data: &[u8]) -> Result<(RequestLine, usize), ParseError> {
@@ -181,5 +203,62 @@ mod tests {
         let result = parse_request_line(b"GET / HTTP/1.1 foobar\r\n");
         assert!(result.is_err());
         println!("result {:#?}", result);
+    }
+    #[test]
+    fn test_full_request() {
+        let data = concat!(
+            "POST /submit HTTP/1.1\r\n",
+            "Host: localhost:42069\r\n",
+            "Content-Length: 13\r\n",
+            "\r\n",
+            "hello world!\n",
+        );
+        let mut reader = ChunkReader::new(data, 3);
+        let request = request_from_reader(&mut reader).expect("request should parse successfully");
+        let request_line = request.request_line.expect("Should have a request line");
+        let host = request
+            .headers
+            .get("host")
+            .expect("Should have host header");
+        assert_eq!(request_line.method, "POST");
+        assert_eq!(request_line.request_target, "/submit");
+        assert_eq!(host, "localhost:42069");
+        assert_eq!(request.body, b"hello world!\n");
+    }
+
+    struct ChunkReader {
+        data: Vec<u8>,
+        position: usize,
+        num_bytes_per_read: usize,
+    }
+
+    impl ChunkReader {
+        fn new(data: &str, num_bytes_per_read: usize) -> Self {
+            Self {
+                data: data.as_bytes().to_vec(),
+                position: 0,
+                num_bytes_per_read,
+            }
+        }
+    }
+
+    impl Read for ChunkReader {
+        fn read(&mut self, buffer: &mut [u8]) -> io::Result<usize> {
+            if self.position >= self.data.len() {
+                return Ok(0); // EOF
+            }
+
+            let remaining = &self.data[self.position..];
+
+            let amount = remaining
+                .len()
+                .min(self.num_bytes_per_read)
+                .min(buffer.len());
+
+            buffer[..amount].copy_from_slice(&remaining[..amount]);
+            self.position += amount;
+
+            Ok(amount)
+        }
     }
 }
